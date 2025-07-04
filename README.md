8214084725fd0f699880c77bf32b93b25c4d2f7f
natanfts <natanaelfreitas1997@gmail.com>
2025-07-04 00:19:32 -0400

    readme adicionado dia 2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 60 dias de programação em Python
 
 - [x] Dia 1 - Olá Mundo em Python
-- [ ] Dia 2
+- [x] Dia 2 - Soma de dois numeros de duas formas diferentes
+- [ ] Dia 3 -
---
+++ day2/somar_dois_numeros.py
@@ -0,0 +1,6 @@
+numero1 = input("Digite um numero:")
+numero2 = input("Digite outro numero:")
+
+soma_dois_numeros = int(numero1) + int(numero2)
+
+print(f"Esssa é a soma de dois numeros em python: {soma_dois_numeros}")
