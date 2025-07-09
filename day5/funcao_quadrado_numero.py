c56fdddb7a327214ce4da8dd53ad1d0eed81d196
natanfts <natanaelfreitas1997@gmail.com>
2025-07-08 20:54:24 -0400

    adicionei o dia 5, função quarado

---
+++ day5/funcao_quadrado_numero.py
@@ -0,0 +1,9 @@
+# Aqui está uma função em simples em Python que recebe um numero e retorna seu quadrado
+def  quadrado_numero(numero):
+    return numero ** 2
+
+def quadrado_numerov2(numero):
+    return numero * numero
+
+print(quadrado_numero(10))
+print(quadrado_numerov2(10))
