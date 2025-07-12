6460c15527e0b1a59b120b255394d011029d3519
natanfts <natanaelfreitas1997@gmail.com>
2025-07-11 20:51:54 -0400

    adicionando o dia 7 dicionarios dinamico e estático

---
+++ day7/dicionario.aluno.py
@@ -0,0 +1,13 @@
+#dicionarios em python
+
+aluno = {
+    "nome": "Natan",
+    "idade": 20,
+    "Curso": "Python do Zero ao Hacking - Ackerdemy",
+    "matricula": 398198413
+}
+
+print("Nome:", aluno["nome"])
+print("idade:", aluno["idade"])
+print("Curso", aluno["Curso"])
+print("matricula", aluno["matricula"])
