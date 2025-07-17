302cc20e47cc9b70c6a24db88052033e1d826f1c
natanfts <natanaelfreitas1997@gmail.com>
2025-07-16 20:25:11 -0400

    adicionamos dia 11, função sorted para listas

---
+++ day11/ordem_lista.py
@@ -0,0 +1,13 @@
+# lista ordenada a partir do metodo sorted()
+
+# 1. lista numerica
+
+lista_num = [55,69,31,66,77,121,661,1,2,3,4]
+
+#sorted() é uma função embutida no python
+#aceita uma lista e retorna ela em ordem
+numeros_ordenados = sorted(lista_num)
+numeros_ordenados2 = sorted(lista_num, reverse=True)
+
+print(numeros_ordenados)
+print(numeros_ordenados2)
